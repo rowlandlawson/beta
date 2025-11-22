e72bea58ed7171387e474c3cd5001570c1d729aa
dextasynergyservices <hellodexta@gmail.com>
2025-11-22 17:04:47 +0100

    create new file

--- print-press-backend/src/index.js
+++ print-press-backend/src/index.js
@@ -26,16 +26,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-// app.use(cors({
-//   origin: process.env.FRONTEND_URL || https://beta-digital-service.vercel.app' || 'http://localhost:3000',
-//   credentials: true,
-// }));
 app.use(cors({
-  origin: [
-    'https://beta-digital-service.vercel.app',
-    'https://beta-ebon-two.vercel.app',
-    'http://localhost:3000'
-  ],
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true,
 }));
 
